21d12cf7ce89a7360b30f1910b296e3758779563
manomya <manomya83@gmail.com>
2026-07-02 23:38:57 +0530

    Allow public access to images folder in NextAuth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,8 +31,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - images (public image assets)
      * - login (login page)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|images|login).*)",
   ],
 };
